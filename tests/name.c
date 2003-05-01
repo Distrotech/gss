35ae7495c56d24a14ce5c17fa962bbb5576ea479
Simon Josefsson <simon@josefsson.org>
2003-05-01 20:40:21 +0000

    Typo.

--- tests/name.c
+++ tests/name.c
@@ -1,20 +1,20 @@
-/* gss.c	Shishi GSS-API self tests.
+/* gss.c	GPL GSS-API GSS-API self tests.
  * Copyright (C) 2003  Simon Josefsson
  *
- * This file is part of Shishi.
+ * This file is part of GPL GSS-API.
  *
- * Shishi is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * GPL GSS-API is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
  *
- * Shishi is distributed in the hope that it will be useful,
+ * GPL GSS-API is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Shishi; if not, write to the Free Software
+ * License along with GPL GSS-API; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  */
