61af13c6ce0f0000917ea32d7021019faca371b0
Simon Josefsson <simon@josefsson.org>
2003-05-08 16:10:38 +0000

    Add.

---
+++ lib/krb5.c
@@ -0,0 +1,30 @@
+/* krb5.c	Implementation of Kerberos 5 GSS functions.
+ * Copyright (C) 2003  Simon Josefsson
+ *
+ * This file is part of GPL GSS-API.
+ *
+ * GPL GSS-API is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GPL GSS-API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GPL GSS-API; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#include "internal.h"
+
+#ifdef USE_KERBEROS5
+
+#include "gss_asn1_tab.c"
+
+
+
+#endif /* USE_KERBEROS5 */
