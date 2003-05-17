d8077472eb20bb1f736a1ae78710b05ac1494de1
Simon Josefsson <simon@josefsson.org>
2003-05-17 22:35:37 +0000

    Rewrite without libtasn1.

--- lib/asn1.c
+++ lib/asn1.c
@@ -1,4 +1,4 @@
-/* asn1.c	Wrapper around ASN.1 code for GSS.
+/* asn1.c	Wrapper around pseudo-ASN.1 token format.
  * Copyright (C) 2003  Simon Josefsson
  *
  * This file is part of the Generic Security Service (GSS).
@@ -20,89 +20,185 @@
  */
 
 #include "internal.h"
-#include "gss_asn1_tab.c"
 
-#define WARN(msg) fprintf(stderr, "gss: libtasn1 error: %s", msg)
+/* RFC 2743:
+ *
+ * 3.1: Mechanism-Independent Token Format
+ *
+ *    This section specifies a mechanism-independent level of encapsulating
+ *    representation for the initial token of a GSS-API context
+ *    establishment sequence, incorporating an identifier of the mechanism
+ *    type to be used on that context and enabling tokens to be interpreted
+ *    unambiguously at GSS-API peers. Use of this format is required for
+ *    initial context establishment tokens of Internet standards-track
+ *    GSS-API mechanisms; use in non-initial tokens is optional.
+ *
+ *    The encoding format for the token tag is derived from ASN.1 and DER
+ *    (per illustrative ASN.1 syntax included later within this
+ *    subsection), but its concrete representation is defined directly in
+ *    terms of octets rather than at the ASN.1 level in order to facilitate
+ *    interoperable implementation without use of general ASN.1 processing
+ *    code.  The token tag consists of the following elements, in order:
+ *
+ *       1. 0x60 -- Tag for [APPLICATION 0] SEQUENCE; indicates that
+ *       -- constructed form, definite length encoding follows.
+ *
+ *       2. Token length octets, specifying length of subsequent data
+ *       (i.e., the summed lengths of elements 3-5 in this list, and of the
+ *       mechanism-defined token object following the tag).  This element
+ *       comprises a variable number of octets:
+ *
+ *          2a. If the indicated value is less than 128, it shall be
+ *          represented in a single octet with bit 8 (high order) set to
+ *          "0" and the remaining bits representing the value.
+ *
+ *          2b. If the indicated value is 128 or more, it shall be
+ *          represented in two or more octets, with bit 8 of the first
+ *          octet set to "1" and the remaining bits of the first octet
+ *          specifying the number of additional octets.  The subsequent
+ *          octets carry the value, 8 bits per octet, most significant
+ *          digit first.  The minimum number of octets shall be used to
+ *          encode the length (i.e., no octets representing leading zeros
+ *          shall be included within the length encoding).
+ *
+ *       3. 0x06 -- Tag for OBJECT IDENTIFIER
+ *
+ *       4. Object identifier length -- length (number of octets) of
+ *       -- the encoded object identifier contained in element 5,
+ *       -- encoded per rules as described in 2a. and 2b. above.
+ *
+ *       5. Object identifier octets -- variable number of octets,
+ *       -- encoded per ASN.1 BER rules:
+ *
+ *          5a. The first octet contains the sum of two values: (1) the
+ *          top-level object identifier component, multiplied by 40
+ *          (decimal), and (2) the second-level object identifier
+ *          component.  This special case is the only point within an
+ *          object identifier encoding where a single octet represents
+ *          contents of more than one component.
+ *
+ *          5b. Subsequent octets, if required, encode successively-lower
+ *          components in the represented object identifier.  A component's
+ *          encoding may span multiple octets, encoding 7 bits per octet
+ *          (most significant bits first) and with bit 8 set to "1" on all
+ *          but the final octet in the component's encoding.  The minimum
+ *          number of octets shall be used to encode each component (i.e.,
+ *          no octets representing leading zeros shall be included within a
+ *          component's encoding).
+ *
+ *       (Note: In many implementations, elements 3-5 may be stored and
+ *       referenced as a contiguous string constant.)
+ *
+ *    The token tag is immediately followed by a mechanism-defined token
+ *    object.  Note that no independent size specifier intervenes following
+ *    the object identifier value to indicate the size of the mechanism-
+ *    defined token object.  While ASN.1 usage within mechanism-defined
+ *    tokens is permitted, there is no requirement that the mechanism-
+ *    specific innerContextToken, innerMsgToken, and sealedUserData data
+ *    elements must employ ASN.1 BER/DER encoding conventions.
+ *
+ *    The following ASN.1 syntax is included for descriptive purposes only,
+ *    to illustrate structural relationships among token and tag objects.
+ *    For interoperability purposes, token and tag encoding shall be
+ *    performed using the concrete encoding procedures described earlier in
+ *    this subsection.
+ *
+ *       GSS-API DEFINITIONS ::=
+ *
+ *       BEGIN
+ *
+ *       MechType ::= OBJECT IDENTIFIER
+ *       -- data structure definitions
+ *       -- callers must be able to distinguish among
+ *       -- InitialContextToken, SubsequentContextToken,
+ *       -- PerMsgToken, and SealedMessage data elements
+ *       -- based on the usage in which they occur
+ *
+ *       InitialContextToken ::=
+ *       -- option indication (delegation, etc.) indicated within
+ *       -- mechanism-specific token
+ *       [APPLICATION 0] IMPLICIT SEQUENCE {
+ *               thisMech MechType,
+ *               innerContextToken ANY DEFINED BY thisMech
+ *                  -- contents mechanism-specific
+ *                  -- ASN.1 structure not required
+ *               }
+ *
+ *       SubsequentContextToken ::= innerContextToken ANY
+ *       -- interpretation based on predecessor InitialContextToken
+ *       -- ASN.1 structure not required
+ *
+ *       PerMsgToken ::=
+ *       -- as emitted by GSS_GetMIC and processed by GSS_VerifyMIC
+ *       -- ASN.1 structure not required
+ *               innerMsgToken ANY
+ *
+ *       SealedMessage ::=
+ *       -- as emitted by GSS_Wrap and processed by GSS_Unwrap
+ *       -- includes internal, mechanism-defined indicator
+ *       -- of whether or not encrypted
+ *       -- ASN.1 structure not required
+ *               sealedUserData ANY
+ *
+ *       END
+ */
+
+/*
+ * The following function borrowed from libtasn.1, under LGPL.
+ * Copyright (C) 2002  Fabio Fiorina
+ */
+static void
+_gss_asn1_length_der(size_t len, unsigned char *ans, size_t *ans_len)
+{
+  size_t k;
+  unsigned char temp[sizeof(len)];
+
+  if(len<128)
+    {
+      if(ans!=NULL) ans[0]=(unsigned char)len;
+      *ans_len=1;
+    }
+  else
+    {
+      k=0;
+
+      while(len)
+	{
+	  temp[k++]=len&0xFF;
+	  len=len>>8;
+	}
+
+      *ans_len=k+1;
+
+      if(ans!=NULL)
+	{
+	  ans[0]=((unsigned char)k&0x7F)+128;
+	  while(k--)
+	    ans[*ans_len-1-k]=temp[k];
+	}
+    }
+}
 
 int
-_gss_wrap_token (char *oid, char *in, size_t inlen, char **out, size_t *outlen)
+_gss_wrap_token (char *oid, size_t oidlen,
+		 char *in, size_t inlen,
+		 char **out, size_t *outlen)
 {
-  ASN1_TYPE definitions = NULL;
-  ASN1_TYPE token = NULL;
-  char errorDescription[MAX_ERROR_DESCRIPTION_SIZE];
-  char *der;
-  int len;
+  size_t asn1len, asn1lenlen;
   int rc;
 
-  rc = asn1_array2tree (gss_asn1_tab, &definitions, errorDescription);
-  if (rc != ASN1_SUCCESS)
-    {
-      WARN (errorDescription);
-      WARN (libtasn1_strerror(rc));
-      return 0;
-    }
+  asn1len = oidlen + inlen;
+  _gss_asn1_length_der(asn1len, NULL, &asn1lenlen);
 
-  rc = asn1_create_element (definitions, "GSS-API.InitialContextToken",
-			    &token);
-  if (rc != ASN1_SUCCESS)
-    {
-      WARN (libtasn1_strerror (rc));
-      return 0;
-    }
-
-  rc = asn1_write_value (token, "thisMech",
-			 oid, 1);
-  if (rc != ASN1_SUCCESS)
-    {
-      WARN (libtasn1_strerror (rc));
-      return 0;
-    }
-
-  rc = asn1_write_value (token, "innerContextToken",
-			 in, inlen);
-  if (rc != ASN1_SUCCESS)
-    {
-      WARN (libtasn1_strerror (rc));
-      return 0;
-    }
-
-  len = 0;
-  rc = asn1_der_coding (token, "", NULL, &len,
-			errorDescription);
-  if (rc != ASN1_MEM_ERROR)
-    {
-      WARN (libtasn1_strerror (rc));
-      return 0;
-    }
-
-  der = malloc(len);
-  if (!der)
+  *outlen = 1 + asn1lenlen + asn1len;
+  *out = malloc(*outlen);
+  if (!*out)
     return 0;
 
-  rc = asn1_der_coding (token, "", der, &len,
-			errorDescription);
-  if (rc != ASN1_SUCCESS)
-    {
-      WARN(libtasn1_strerror (rc));
-      free(der);
-      return 0;
-    }
-
-#if 0
-  /* This doesn't appear to work (warn/crashes).
-     XXX Memory leak as a result? */
-
-  rc = asn1_delete_structure(token);
-  if (rc != ASN1_SUCCESS)
-    WARN(libtasn1_strerror (rc));
-
-  rc = asn1_delete_structure(definitions);
-  if (rc != ASN1_SUCCESS)
-    WARN(libtasn1_strerror (rc));
-#endif
-
-  *out = der;
-  *outlen = len;
+  **out = '\x60';
+  _gss_asn1_length_der(asn1len, *out + 1, &asn1lenlen);
+  memcpy(*out + 1 + asn1lenlen, oid, oidlen);
+  memcpy(*out + 1 + asn1lenlen + oidlen, in, inlen);
 
   return 1;
 }
