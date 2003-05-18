b419db3389863a10058213d5f1a10663dc0f3661
Simon Josefsson <simon@josefsson.org>
2003-05-18 15:44:51 +0000

    Cleanup.

--- lib/krb5.c
+++ lib/krb5.c
@@ -23,75 +23,26 @@
 
 #ifdef USE_KERBEROS5
 
-/*
- * From RFC 1964:
- *
- * To support ongoing experimentation, testing, and evolution of the
- * specification, the Kerberos V5 GSS-API mechanism as defined in this
- * and any successor memos will be identified with the following
- * Object Identifier, as defined in RFC-1510, until the specification
- * is advanced to the level of Proposed Standard RFC:
- *
- * {iso(1), org(3), dod(5), internet(1), security(5), kerberosv5(2)}
- *
- * Upon advancement to the level of Proposed Standard RFC, the
- * Kerberos V5 GSS-API mechanism will be identified by an Object
- * Identifier having the value:
- *
- * {iso(1) member-body(2) United States(840) mit(113554) infosys(1)
- * gssapi(2) krb5(2)}
- *
- */
+#define _GSS_KRB5_OID "\x2a\x86\x48\x86\xf7\x12\x01\x02\x02"
+#define _GSS_KRB5_OID_STRING "1.2.840.113554.1.2.2"
 
-#define _GSS_KRB5_OID "1.2.840.113554.1.2.2"
+gss_OID_desc GSS_KRB5_NT_PRINCIPAL_NAME_static = {
+  9, (void *) _GSS_KRB5_OID
+};
+gss_OID GSS_KRB5_NT_PRINCIPAL_NAME = &GSS_KRB5_NT_PRINCIPAL_NAME_static;
 
-/*
- * From RFC 1964:
- *
- * 1.1. Context Establishment Tokens
- *
- *    Per RFC-1508, Appendix B, the initial context establishment token
- *    will be enclosed within framing as follows:
- *
- *    InitialContextToken ::=
- *    [APPLICATION 0] IMPLICIT SEQUENCE {
- *            thisMech        MechType
- *                    -- MechType is OBJECT IDENTIFIER
- *                    -- representing "Kerberos V5"
- *            innerContextToken ANY DEFINED BY thisMech
- *                    -- contents mechanism-specific;
- *                    -- ASN.1 usage within innerContextToken
- *                    -- is not required
- *            }
- *
- *    The innerContextToken of the initial context token will consist of a
- *    Kerberos V5 KRB_AP_REQ message, preceded by a two-byte token-id
- *    (TOK_ID) field, which shall contain the value 01 00.
- *
- *    The above GSS-API framing shall be applied to all tokens emitted by
- *    the Kerberos V5 GSS-API mechanism, including KRB_AP_REP, KRB_ERROR,
- *    context-deletion, and per-message tokens, not just to the initial
- *    token in a context establishment sequence.  While not required by
- *    RFC-1508, this enables implementations to perform enhanced error-
- *    checking. The innerContextToken field of context establishment tokens
- *    for the Kerberos V5 GSS-API mechanism will contain a Kerberos message
- *    (KRB_AP_REQ, KRB_AP_REP or KRB_ERROR), preceded by a 2-byte TOK_ID
- *    field containing 01 00 for KRB_AP_REQ messages, 02 00 for KRB_AP_REP
- *    messages and 03 00 for KRB_ERROR messages.
- *
- */
+#define _GSS_KRB5_OID_DER "\x06\x09" _GSS_KRB5_OID
+#define _GSS_KRB5_OID_LEN  strlen(_GSS_KRB5_OID_DER)
 
-#define _GSS_KRB5_OID_DATA "\x06\x09\x2a\x86\x48\x86\xf7\x12\x01\x02\x02"
-#define _GSS_KRB5_OID_LEN  strlen(_GSS_KRB5_OID_DATA)
-
-#define _GSS_KRB5_AP_REQ_DATA    _GSS_KRB5_OID_DATA "\x01\x00"
+#define _GSS_KRB5_AP_REQ_DATA    _GSS_KRB5_OID_DER "\x01\x00"
 #define _GSS_KRB5_AP_REQ_LEN     (_GSS_KRB5_OID_LEN+2)
 
-#define _GSS_KRB5_AP_REP_DATA    _GSS_KRB5_OID_DATA "\x02\x00"
+#define _GSS_KRB5_AP_REP_DATA    _GSS_KRB5_OID_DER "\x02\x00"
 #define _GSS_KRB5_AP_REP_LEN     (_GSS_KRB5_OID_LEN+2)
 
-#define _GSS_KRB5_KRB_ERROR_DATA _GSS_KRB5_OID_DATA "\x03\x00"
+#define _GSS_KRB5_KRB_ERROR_DATA _GSS_KRB5_OID_DER "\x03\x00"
 #define _GSS_KRB5_KRB_ERROR_LEN  (_GSS_KRB5_OID_LEN+2)
+
 
 OM_uint32
 krb5_gss_init_sec_context (OM_uint32 * minor_status,
@@ -113,8 +64,23 @@ krb5_gss_init_sec_context (OM_uint32 * minor_status,
   char *data;
   size_t len;
   int rc;
+  gss_name_desc sname;
+  gss_name_t snameptr = &sname;
+  OM_uint32 maj_stat;
 
-  target_name->value = "imap/latte-wlan.josefsson.org";
+  if (target_name->type != GSS_C_NT_EXPORT_NAME) /* XXX ptr compare? */
+    {
+      maj_stat = gss_canonicalize_name (minor_status, target_name,
+					mech_type, &snameptr);
+    }
+  else
+    {
+      maj_stat = gss_duplicate_name (minor_status, target_name, &snameptr);
+    }
+  if (maj_stat != GSS_S_COMPLETE)
+    return maj_stat;
+
+  /* XXX release_name (sname) */
 
   /* Note: mech_type not tested */
 
@@ -141,7 +107,7 @@ krb5_gss_init_sec_context (OM_uint32 * minor_status,
 	  Shishi_tkts_hint hint;
 
 	  memset(&hint, 0, sizeof(hint));
-	  hint.server = target_name->value;
+	  hint.server = sname.value;
 
 	  tkt = shishi_tkts_get (shishi_tkts_default (h), &hint);
 	  if (!tkt)
@@ -228,8 +194,78 @@ krb5_gss_init_sec_context (OM_uint32 * minor_status,
 	  }
       }
 
-      puts(target_name->value);
     }
+
+  return GSS_S_COMPLETE;
+}
+
+static OM_uint32
+_krb5_gss_canonicalize_name_1 (OM_uint32 * minor_status,
+			       const gss_name_t input_name,
+			       const gss_OID mech_type,
+			       gss_name_t * output_name,
+			       gss_OID_set oid_set)
+{
+  OM_uint32 major_stat;
+  int present;
+
+  major_stat = gss_test_oid_set_member (minor_status,
+					GSS_C_NT_HOSTBASED_SERVICE,
+					oid_set, &present);
+  if (major_stat != GSS_S_COMPLETE)
+    return major_stat;
+
+  if (present)
+    {
+      char *p;
+
+      /* XXX we don't do DNS name canoncalization */
+
+      (*output_name)->value = strdup(input_name->value);
+      (*output_name)->length = strlen((*output_name)->value);
+      (*output_name)->type = GSS_KRB5_NT_PRINCIPAL_NAME;
+
+      if ((p = strchr((*output_name)->value, '@')))
+	{
+	  *p = '/';
+	}
+      else
+	{
+	  /* XXX add "/gethostname()" to outputname->value */
+	}
+    }
+  else
+    return GSS_S_FAILURE;
+
+  return GSS_S_COMPLETE;
+}
+
+OM_uint32
+krb5_gss_canonicalize_name (OM_uint32 * minor_status,
+			    const gss_name_t input_name,
+			    const gss_OID mech_type,
+			    gss_name_t * output_name)
+{
+  OM_uint32 major_stat;
+  gss_OID_set oid_set;
+
+  major_stat = gss_create_empty_oid_set (minor_status, &oid_set);
+  if (major_stat != GSS_S_COMPLETE)
+    return major_stat;
+
+  major_stat = gss_add_oid_set_member (minor_status, input_name->type,
+				       &oid_set);
+  if (major_stat != GSS_S_COMPLETE)
+    return major_stat;
+
+  _krb5_gss_canonicalize_name_1 (minor_status, input_name, mech_type,
+				 output_name, oid_set);
+  if (major_stat != GSS_S_COMPLETE)
+    return major_stat;
+
+  major_stat = gss_release_oid_set (minor_status, &oid_set);
+  if (major_stat != GSS_S_COMPLETE)
+    return major_stat;
 
   return GSS_S_COMPLETE;
 }
