2d021cc74ef791b65218d4ceeff066110ace2e1b
Simon Josefsson <simon@josefsson.org>
2003-05-29 20:48:01 +0000

    Add.

---
+++ lib/ext.h
@@ -0,0 +1,38 @@
+/* ext.h	Header file for non-standard GSS-API functions.
+ * Copyright (C) 2003  Simon Josefsson
+ *
+ * This file is part of the Generic Security Service (GSS).
+ *
+ * GSS is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GSS is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GSS; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+#ifndef GSS_EXT_H_
+#define GSS_EXT_H_
+
+extern const char *
+gss_check_version (const char *req_version);
+
+extern int
+gss_encapsulate_token (gss_buffer_t input_message,
+		       gss_OID token_oid,
+		       gss_buffer_t output_message);
+
+extern int
+gss_decapsulate_token (gss_buffer_t input_message,
+		       gss_OID token_oid,
+		       gss_buffer_t output_message);
+
+#endif
