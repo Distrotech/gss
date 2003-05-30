e59381a155cb38ed003a412fe6308535533161b0
Simon Josefsson <simon@josefsson.org>
2003-05-30 01:10:11 +0000

    Add.

--- lib/ext.h
+++ lib/ext.h
@@ -29,6 +29,10 @@ extern int
 gss_oid_equal (gss_OID first_oid, gss_OID second_oid);
 
 extern OM_uint32
+gss_copy_oid (OM_uint32 * minor_status,
+	      const gss_OID src_oid, gss_OID dest_oid);
+
+extern OM_uint32
 gss_duplicate_oid (OM_uint32 * minor_status,
 		   const gss_OID src_oid, gss_OID * dest_oid);
 
