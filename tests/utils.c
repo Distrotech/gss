e2540711343b206819068080f4d7e5979886ac58
Simon Josefsson <simon@josefsson.org>
2003-06-10 10:23:31 +0000

    Fix.

--- tests/utils.c
+++ tests/utils.c
@@ -1,21 +1,21 @@
-/* utils.c	GPL GSS-API self tests utilities.
+/* utils.c	GSS self tests utilities.
  * Copyright (C) 2002, 2003  Simon Josefsson
  *
- * This file is part of GPL GSS-API.
+ * This file is part of the Generic Security Service (GSS).
  *
- * GPL GSS-API is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
+ * GSS is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * GPL GSS-API is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * GSS is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with GPL GSS-API; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU General Public License
+ * along with GSS; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  */
 
