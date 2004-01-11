8a37d044df8a6355b5fc88dd1215ce7ea8fc9a9e
Simon Josefsson <simon@josefsson.org>
2004-01-11 04:35:55 +0000

    Add gss_release_oid. Error fixes. Doc fix.

--- lib/misc.c
+++ lib/misc.c
@@ -21,10 +21,24 @@
 
 #include "internal.h"
 
+/**
+ * gss_oid_equal:
+ * @first_oid: (Object ID, read) First Object identifier.
+ * @second_oid: (Object ID, read) First Object identifier.
+ *
+ * Compare two OIDs for equality.  The comparison is "deep", i.e., the
+ * actual byte sequences of the OIDs are compared instead of just the
+ * pointer equality.
+ *
+ * WARNING: This function is a GNU GSSLib specific extension, and is
+ * not part of the official GSS API.
+ *
+ * Return value: Returns boolean value true when the two OIDs are
+ *   equal, otherwise false.
+ **/
 int
 gss_oid_equal (gss_OID first_oid, gss_OID second_oid)
 {
-  /* This function is not part of the official GSS API */
 
   return first_oid && second_oid &&
     first_oid->length == second_oid->length &&
@@ -32,17 +46,38 @@ gss_oid_equal (gss_OID first_oid, gss_OID second_oid)
 	    second_oid->length) == 0;
 }
 
+/**
+ * gss_copy_oid:
+ * @minor_status: (integer, modify) Mechanism specific status code.
+ * @src_oid: (Object ID, read) The object identifier to copy.
+ * @dest_oid: (Object ID, modify) The resultant copy of @src_oid.
+ *   Storage associated with this name must be freed by the
+ *   application, but gss_release_oid() cannot be used generally as it
+ *   deallocate the the oid structure itself too (use
+ *   gss_duplicate_oid() if you don't want this problem.)
+ *
+ * Make an exact copy of the given OID, that shares no memory areas
+ * with the original.
+ *
+ * WARNING: This function is a GNU GSSLib specific extension, and is
+ * not part of the official GSS API.
+ *
+ * Valid return values and their meaning:
+ *
+ * `GSS_S_COMPLETE`: Successful completion.
+ **/
 OM_uint32
 gss_copy_oid (OM_uint32 * minor_status,
 	      const gss_OID src_oid, gss_OID dest_oid)
 {
-  /* This function is not part of the official GSS API */
-
   if (minor_status)
     *minor_status = 0;
 
-  if (!src_oid || src_oid->length == 0 || src_oid->elements == NULL)
-    return GSS_S_FAILURE;
+  if (!src_oid)
+    return GSS_S_FAILURE | GSS_S_CALL_INACCESSIBLE_READ;
+
+  if (src_oid->length == 0 || src_oid->elements == NULL)
+    return GSS_S_FAILURE | GSS_S_CALL_BAD_STRUCTURE;
 
   dest_oid->length = src_oid->length;
   dest_oid->elements = xmalloc (src_oid->length);
@@ -51,6 +86,24 @@ gss_copy_oid (OM_uint32 * minor_status,
   return GSS_S_COMPLETE;
 }
 
+/**
+ * gss_duplicate_oid:
+ * @minor_status: (integer, modify) Mechanism specific status code.
+ * @src_oid: (Object ID, read) The object identifier to duplicate.
+ * @dest_oid: (Object ID, modify) The resultant copy of @src_oid.
+ *   Storage associated with this name must be freed by the
+ *   application, by calling gss_release_oid().
+ *
+ * Allocate a new OID and make it an exact copy of the given OID, that
+ * shares no memory areas with the original.
+ *
+ * WARNING: This function is a GNU GSSLib specific extension, and is
+ * not part of the official GSS API.
+ *
+ * Valid return values and their meaning:
+ *
+ * `GSS_S_COMPLETE`: Successful completion.
+ **/
 OM_uint32
 gss_duplicate_oid (OM_uint32 * minor_status,
 		   const gss_OID src_oid, gss_OID * dest_oid)
@@ -62,16 +115,54 @@ gss_duplicate_oid (OM_uint32 * minor_status,
   if (minor_status)
     *minor_status = 0;
 
-  if (!src_oid || src_oid->length == 0 || src_oid->elements == NULL)
-    return GSS_S_FAILURE;
+  if (!src_oid)
+    return GSS_S_FAILURE | GSS_S_CALL_INACCESSIBLE_READ;
+
+  if (src_oid->length == 0 || src_oid->elements == NULL)
+    return GSS_S_FAILURE | GSS_S_CALL_BAD_STRUCTURE;
 
   *dest_oid = xmalloc (sizeof (**dest_oid));
 
   maj_stat = gss_copy_oid (minor_status, src_oid, *dest_oid);
-  if (maj_stat != GSS_S_COMPLETE)
+  if (GSS_ERROR (maj_stat))
     {
       free (*dest_oid);
       return maj_stat;
+    }
+
+  return GSS_S_COMPLETE;
+}
+
+/**
+ * gss_release_oid:
+ * @minor_status: (Integer, modify) Mechanism specific status code.
+ * @oid: (gss_name_t, modify) The OID to be deleted.
+ *
+ * Free GSSAPI-allocated storage associated with an OID.  The OID is
+ * set to GSS_C_NO_NAME on successful completion of this call.
+ *
+ * WARNING: This function is a GNU GSSLib specific extension, and is
+ * not part of the official GSS API.
+ *
+ * Valid return values and their meaning:
+ *
+ * `GSS_S_COMPLETE`: Successful completion.
+ **/
+OM_uint32
+gss_release_oid (OM_uint32 * minor_status, gss_OID *oid)
+{
+  if (minor_status)
+    *minor_status = 0;
+
+  if (!oid)
+    return GSS_S_FAILURE | GSS_S_CALL_INACCESSIBLE_READ;
+
+  if (*oid != GSS_C_NO_OID)
+    {
+      if ((*oid)->elements)
+	free ((*oid)->elements);
+      free (*oid);
+      *oid = GSS_C_NO_OID;
     }
 
   return GSS_S_COMPLETE;
