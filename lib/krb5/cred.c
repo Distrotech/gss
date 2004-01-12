9eed0a4ce9fedf47bebe6e3452c42375d902ca67
Simon Josefsson <simon@josefsson.org>
2004-01-12 02:40:22 +0000

    Fix.

--- lib/krb5/cred.c
+++ lib/krb5/cred.c
@@ -142,40 +142,19 @@ gss_krb5_inquire_cred (OM_uint32 * minor_status,
 		       gss_OID_set * mechanisms)
 {
   OM_uint32 maj_stat;
-  gss_cred_id_t local_cred_handle = GSS_C_NO_CREDENTIAL;
-
-  if (minor_status)
-    *minor_status = 0;
 
   if (cred_handle == GSS_C_NO_CREDENTIAL)
+    return GSS_S_NO_CRED;
+
+  if (mechanisms)
     {
-      /* XXX Assumes we get a krb5 credential. */
-      maj_stat = gss_acquire_cred (minor_status, GSS_C_NO_NAME,
-				   GSS_C_INDEFINITE,
-				   GSS_C_NO_OID_SET,
-				   GSS_C_INITIATE,
-				   &local_cred_handle,
-				   mechanisms,
-				   lifetime);
+      maj_stat = gss_create_empty_oid_set (minor_status, mechanisms);
       if (GSS_ERROR (maj_stat))
 	return maj_stat;
-      cred_handle = local_cred_handle;
-    }
-  else
-    {
-      if (mechanisms)
-	{
-	  maj_stat = gss_create_empty_oid_set (minor_status, mechanisms);
-	  if (GSS_ERROR (maj_stat))
-	    return maj_stat;
-	  maj_stat = gss_add_oid_set_member (minor_status, GSS_KRB5,
-					     mechanisms);
-	  if (GSS_ERROR (maj_stat))
-	    return maj_stat;
-	}
-
-      if (lifetime)
-	*lifetime = gss_krb5_tktlifetime (cred_handle->krb5->tkt);
+      maj_stat = gss_add_oid_set_member (minor_status, GSS_KRB5,
+					 mechanisms);
+      if (GSS_ERROR (maj_stat))
+	return maj_stat;
     }
 
   if (name)
@@ -186,9 +165,14 @@ gss_krb5_inquire_cred (OM_uint32 * minor_status,
 	return maj_stat;
     }
 
+  if (lifetime)
+    *lifetime = gss_krb5_tktlifetime (cred_handle->krb5->tkt);
+
   if (cred_usage)
     *cred_usage = GSS_C_BOTH;
 
+  if (minor_status)
+    *minor_status = 0;
   return GSS_S_COMPLETE;
 }
 
