08b78949279c76e1c215cad23ae97a82ed4a2ba9
Simon Josefsson <simon@josefsson.org>
2004-01-12 03:20:37 +0000

    (_gss_mech_apis): Add gss_krb5_inquire_cred_by_mech. (_gss_find_mech): Use const for OID.

--- lib/meta.c
+++ lib/meta.c
@@ -29,32 +29,34 @@
 static _gss_mech_api_desc _gss_mech_apis[] = {
 #ifdef USE_KERBEROS5
   {
-   &GSS_KRB5_static,
-   {
-     /* Mandatory name-types. */
-     &GSS_KRB5_NT_PRINCIPAL_NAME_static,
-     &GSS_C_NT_HOSTBASED_SERVICE_static,
-     &GSS_C_NT_EXPORT_NAME_static
-   },
-   gss_krb5_init_sec_context,
-   gss_krb5_canonicalize_name,
-   gss_krb5_export_name,
-   gss_krb5_wrap,
-   gss_krb5_unwrap,
-   gss_krb5_get_mic,
-   gss_krb5_verify_mic,
-   gss_krb5_display_status,
-   gss_krb5_acquire_cred,
-   gss_krb5_release_cred,
-   gss_krb5_accept_sec_context,
-   gss_krb5_delete_sec_context,
-   gss_krb5_context_time,
-   gss_krb5_inquire_cred}
+    &GSS_KRB5_static,
+    {
+      /* Mandatory name-types. */
+      &GSS_KRB5_NT_PRINCIPAL_NAME_static,
+      &GSS_C_NT_HOSTBASED_SERVICE_static,
+      &GSS_C_NT_EXPORT_NAME_static
+    },
+    gss_krb5_init_sec_context,
+    gss_krb5_canonicalize_name,
+    gss_krb5_export_name,
+    gss_krb5_wrap,
+    gss_krb5_unwrap,
+    gss_krb5_get_mic,
+    gss_krb5_verify_mic,
+    gss_krb5_display_status,
+    gss_krb5_acquire_cred,
+    gss_krb5_release_cred,
+    gss_krb5_accept_sec_context,
+    gss_krb5_delete_sec_context,
+    gss_krb5_context_time,
+    gss_krb5_inquire_cred,
+    gss_krb5_inquire_cred_by_mech
+  }
 #endif
 };
 
 _gss_mech_api_t
-_gss_find_mech (gss_OID oid)
+_gss_find_mech (const gss_OID oid)
 {
   size_t i;
 
