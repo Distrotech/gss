04c31b7c017a5fd715d82c161a128f90f1045750
Simon Josefsson <simon@josefsson.org>
2004-01-15 17:44:49 +0000

    Add command line tool in src/. Add getopt in gl/, for gengetopt parser in src/.

---
+++ src/gss.c
@@ -0,0 +1,109 @@
+/* gss.c --- Command line tool for GSS.
+ * Copyright (C) 2004  Simon Josefsson
+ *
+ * This file is part of the Generic Security Service (GSS).
+ *
+ * GSS is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GSS is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GSS; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ */
+
+#if HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <gss.h>
+
+#include "gss_cmd.h"
+
+int
+main (int argc, char *argv[])
+{
+  struct gengetopt_args_info args;
+  gss_buffer_desc status_string;
+  OM_uint32 message_context = 0;
+  OM_uint32 maj, min;
+  size_t i;
+
+  if (cmdline_parser (argc, argv, &args) != 0)
+    return 1;
+
+  if (!args.major_given)
+    {
+      fprintf (stderr, "%s: missing parameter.\n", argv[0]);
+      fprintf (stderr, "Try `%s --help' for more information.\n", argv[0]);
+      cmdline_parser_print_help ();
+      return 1;
+    }
+
+  printf ("GSS-API major status %d (0x%x).\n\n"
+	  "Masked routine error %d (0x%x) shifted into %d (0x%x).\n"
+	  "Masked calling error %d (0x%x) shifted into %d (0x%x).\n"
+	  "Masked supplementary info %d (0x%x) shifted into %d (0x%x).\n\n",
+	  args.major_arg, args.major_arg,
+	  GSS_ROUTINE_ERROR (args.major_arg),
+	  GSS_ROUTINE_ERROR (args.major_arg),
+	  GSS_ROUTINE_ERROR (args.major_arg) >> GSS_C_ROUTINE_ERROR_OFFSET,
+	  GSS_ROUTINE_ERROR (args.major_arg) >> GSS_C_ROUTINE_ERROR_OFFSET,
+	  GSS_CALLING_ERROR (args.major_arg),
+	  GSS_CALLING_ERROR (args.major_arg),
+	  GSS_CALLING_ERROR (args.major_arg) >> GSS_C_CALLING_ERROR_OFFSET,
+	  GSS_CALLING_ERROR (args.major_arg) >> GSS_C_CALLING_ERROR_OFFSET,
+	  GSS_SUPPLEMENTARY_INFO (args.major_arg),
+	  GSS_SUPPLEMENTARY_INFO (args.major_arg),
+	  GSS_SUPPLEMENTARY_INFO (args.major_arg) >> GSS_C_SUPPLEMENTARY_OFFSET,
+	  GSS_SUPPLEMENTARY_INFO (args.major_arg) >> GSS_C_SUPPLEMENTARY_OFFSET);
+
+  printf("   MSB                                                                 LSB\n"
+	 "   +-----------------+-----------------+---------------------------------+\n"
+	 "   |  Calling Error  |  Routine Error  |       Supplementary Info        |\n");
+  printf ("   | ");
+  for (i = 0; i < 8; i++)
+    printf ("%d ", (args.major_arg >> (31-i)) & 1);
+  printf ("| ");
+  for (i = 0; i < 8; i++)
+    printf ("%d ", (args.major_arg >> (23-i)) & 1);
+  printf ("| ");
+  for (i = 0; i < 16; i++)
+    printf ("%d ", (args.major_arg >> (15-i)) & 1);
+  printf ("|\n");
+  printf ("   +-----------------+-----------------+---------------------------------+\n"
+	  "Bit 31            24  23            16  15                             0\n\n");
+
+  printf ("Description:\n");
+  do
+    {
+      maj = gss_display_status (&min, args.major_arg,
+				GSS_C_GSS_CODE, GSS_C_NO_OID,
+				&message_context,
+				&status_string);
+      if (GSS_ERROR (maj))
+	{
+	  fprintf (stderr, "%s: error printing error code\n", argv[0]);
+	  return 1;
+	}
+
+      printf("%.*s\n", (int)status_string.length,
+	     (char *)status_string.value);
+      
+      gss_release_buffer(&min, &status_string);
+    }
+  while (message_context);
+
+  return 0;
+}
