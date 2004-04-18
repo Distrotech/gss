6e6cc9fb7146698febc25224a11bf3d4f57fcbdd
Simon Josefsson <simon@josefsson.org>
2004-04-18 15:48:59 +0000

    Sync with gnulib (and update my changes).

--- gl/xalloc.h
+++ gl/xalloc.h
@@ -1,5 +1,6 @@
 /* xalloc.h -- malloc with out-of-memory checking
 
+   Copyright (C) 2004 Simon Josefsson
    Copyright (C) 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
    1999, 2000, 2003 Free Software Foundation, Inc.
 
