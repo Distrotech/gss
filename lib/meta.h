78d23cd52658a0bc0c785becdee989bb85076124
Simon Josefsson <simon@josefsson.org>
2004-04-19 21:30:27 +0000

    Typo.

--- lib/meta.h
+++ lib/meta.h
@@ -35,8 +35,7 @@ typedef struct _gss_mech_api_struct
      const gss_OID mech_type,
      OM_uint32 req_flags,
      OM_uint32 time_req,
-     const gss_channel_bindings_t
-     input_chan_bindings,
+     const gss_channel_bindings_t input_chan_bindings,
      const gss_buffer_t input_token,
      gss_OID * actual_mech_type,
      gss_buffer_t output_token, OM_uint32 * ret_flags, OM_uint32 * time_rec);
@@ -89,8 +88,7 @@ typedef struct _gss_mech_api_struct
      gss_ctx_id_t * context_handle,
      const gss_cred_id_t acceptor_cred_handle,
      const gss_buffer_t input_token_buffer,
-     const gss_channel_bindings_t
-     input_chan_bindings,
+     const gss_channel_bindings_t input_chan_bindings,
      gss_name_t * src_name,
      gss_OID * mech_type,
      gss_buffer_t output_token,
