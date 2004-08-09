4154a045510b1cf281a35bd34062273994b31086
Simon Josefsson <simon@josefsson.org>
2004-08-09 08:50:06 +0000

    Fix.

--- lib/ext.c
+++ lib/ext.c
@@ -32,6 +32,7 @@ xalloc_die (void)
 {
   if (gss_alloc_fail_function)
     (*gss_alloc_fail_function) ();
+  fflush (stdout);
   fprintf (stderr, "%s: %s\n", PACKAGE, strerror (ENOMEM));
   abort ();
 }
