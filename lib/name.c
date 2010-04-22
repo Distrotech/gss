a28ee55824f55978ddfc452ee85c341404f1568b
Simon Josefsson <simon@josefsson.org>
2010-04-22 09:47:50 +0200

    Bump copyright years.  Fix PGP key.

--- lib/name.c
+++ lib/name.c
@@ -1,5 +1,5 @@
 /* name.c --- Implementation of GSS-API Name Manipulation functions.
- * Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009  Simon Josefsson
+ * Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010  Simon Josefsson
  *
  * This file is part of the Generic Security Service (GSS).
  *
--- lib/oid.c
+++ lib/oid.c
@@ -1,5 +1,5 @@
 /* oid.c --- Definition of static GSS-API OIDs.
- * Copyright (C) 2003, 2004, 2005, 2006, 2007  Simon Josefsson
+ * Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010  Simon Josefsson
  *
  * This file is part of the Generic Security Service (GSS).
  *
