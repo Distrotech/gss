b3fa34bc9a1694ad84e7607d7b052b0a07511f33
Simon Josefsson <simon@josefsson.org>
2012-02-08 13:11:05 +0100

    Fix ignore rule.

--- cfg.mk
+++ cfg.mk
@@ -30,7 +30,7 @@ endif
 local-checks-to-skip = sc_copyright_check sc_immutable_NEWS		\
 	sc_makefile_at_at_check sc_prohibit_strcmp sc_require_config_h	\
 	sc_require_config_h_first
-VC_LIST_ALWAYS_EXCLUDE_REGEX = ^maint.mk|GNUmakefile|gtk-doc.make|po/.*.po.in|((lib/|src/)?(gl|gltests|build-aux))/.*$$
+VC_LIST_ALWAYS_EXCLUDE_REGEX = ^maint.mk|GNUmakefile|gtk-doc.make|po/.*.po.in|doc/fdl-1.3.texi|doc/gendocs_template|m4/pkg.m4|build-aux/|((lib/|src/)?gl)/.*$$
 
 update-copyright-env = UPDATE_COPYRIGHT_HOLDER="Simon Josefsson" UPDATE_COPYRIGHT_USE_INTERVALS=2
 
